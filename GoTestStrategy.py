e9ec06042093bdfceb20faeb0d5688e6b689155d
raniamerzouk <Rania@MacBook-Pro-de-Rania.local>
2019-02-25 19:11:48 +0100

    tme 6

--- GoTestStrategy.py
+++ GoTestStrategy.py
@@ -8,22 +8,22 @@ Created on Mon Feb 18 18:22:46 2019
 from soccersimulator import Strategy, SoccerAction, Vector2D, SoccerTeam, Simulation, show_simu
 from sklearn.model_selection import ParameterGrid
 from rania_fatema.actions import *
-from rania_fatema.GoalSearch import*
+from rania_fatema.GoalSearch import *
+from rania_fatema import *
 import math
 
-class GoTestStrategy ( Strategy ):
-    def __init__ ( self , strength = None ):
-        Strategy.__init__(self, " Go - getter " )
+class GoTestStrategy (Strategy):
+    def __init__ (self , strength = None):
+        Strategy.__init__(self, " Go - getter ")
         self.strength = strength
 
-    def compute_strategy ( self , state , id_team , id_player ):
-        s = SuperState ( state , id_team , id_player )
+    def compute_strategy (self, state, id_team, id_player):
+        s = SuperState(state, id_team, id_player)
         move = Move(s)
         shoot = Shoot(s)
-        return move.to_ball() + shoot.to_goal(self.strength )
+        return move.to_ball() + shoot.to_goal(self.strength)
     
-
-expe = GoalSearch (strategy= GoTestStrategy() ,params ={ 'strength': [0.1 , 1]})
+expe = GoalSearch(strategy = GoTestStrategy(), params = {'strength': [0.1 , 1]})
 expe.start()
 print(expe.get_res())
 print(expe.get_best())
--- rania_fatema/GoalSearch.py
+++ rania_fatema/GoalSearch.py
@@ -12,8 +12,8 @@ from .actions import *
 from sklearn.model_selection import ParameterGrid
 
 class GoalSearch (object):
-    def __init__(self , strategy , params , simu = None , trials = 20,
-                  max_steps =1000000 , max_round_step= 40):
+    def __init__(self, strategy, params, simu = None, trials = 20,
+                  max_steps = 1000000, max_round_step = 40):
         self.strategy = strategy
         self.params = params.copy()
         self.simu = simu
@@ -21,43 +21,43 @@ class GoalSearch (object):
         self.max_steps = max_steps
         self.max_round_step = max_round_step
     
-    def start( self , show = True ):
-        if not self . simu :
-            team1 = SoccerTeam ( " Team ␣ 1 " )
-            team2 = SoccerTeam ( " Team ␣ 2 " )
-            team1.add ( self.strategy.name , self.strategy )
-            team2.add ( Strategy().name , Strategy ())
-            self.simu = Simulation ( team1 , team2 , max_steps = self.max_steps )
+    def start(self , show = True):
+        if not self.simu :
+            team1 = SoccerTeam(" Team ␣ 1 ")
+            team2 = SoccerTeam (" Team ␣ 2 ")
+            team1.add(self.strategy.name, self.strategy)
+            team2.add(Strategy().name, Strategy())
+            self.simu = Simulation (team1, team2, max_steps = self.max_steps)
         self.simu.listeners += self
         
-        if show :
+        if show:
             show_simu(self.simu )
         else:
             self.simu.start()
 
-    def begin_match( self, team1, team2, state ):
+    def begin_match(self, team1, team2, state):
         self.last_step = 0 # Step of the last round
         self.criterion = 0 # Criterion to maximize ( here , number of goals )
         self.cpt_trials = 0 # Counter for trials
-        self.param_grid = iter(ParameterGrid(self.params )) # Iterator for the g
-        self.cur_param = next( self.param_grid , None ) # Current parameter
-        if self.cur_param is None :
+        self.param_grid = iter(ParameterGrid(self.params)) # Iterator for the g
+        self.cur_param = next(self.param_grid , None) # Current parameter
+        if self.cur_param is None:
             raise ValueError('no parameter given.')
         self.res = dict() # Dictionary of results
     
-    def begin_round( self , team1 , team2 , state ):
+    def begin_round(self, team1, team2, state):
         ball = Vector2D.create_random(low =0 , high =1)
         ball.x += 30
         ball.y += GAME_HEIGHT/2
         
         # Player and ball postion ( random )
-        self.simu.state.states [(1 , 0)].position = ball.copy() # Player position
-        self.simu.state.states [(1 , 0)].vitesse = Vector2D() # Player accelerati
+        self.simu.state.states[(1, 0)].position = ball.copy() # Player position
+        self.simu.state.states[(1, 0)].vitesse = Vector2D() # Player accelerati
         self.simu.state.ball.position = ball.copy() # Ball position
         self.last_step = self.simu.step  # Last step of the game
 
         # Set the current value for the current parameters
-        for key,value in self.cur_param.items():
+        for key, value in self.cur_param.items():
             setattr(self.strategy, key, value)
             
     def end_round(self , team1 , team2 , state):
@@ -65,10 +65,10 @@ class GoalSearch (object):
         if state.goal > 0:
             self.criterion += 1 # Increment criterion
         
-        self.cpt_trials += 1# Increment number of trials
-            
-        print(self.cur_param, end = " ␣ ␣ ␣ ␣ ")
-        print(" Crit : ␣ {} ␣ ␣ ␣ Cpt : ␣ {} ". format( self.criterion , self . cpt_trials ))
+        self.cpt_trials += 1 # Increment number of trials
+        
+        print(self.cur_param)
+        print(" Crit : ␣{}␣␣␣ Cpt :␣{} ". format(self.criterion, self.cpt_trials))
 
         if self.cpt_trials >= self.trials :
             # Save the result
@@ -79,16 +79,16 @@ class GoalSearch (object):
             self.cpt_trials = 0
 
             # Next parameter value
-            self.cur_param = next( self.param_grid , None )
+            self.cur_param = next(self.param_grid, None)
             if self.cur_param is None :
                 self.simu.end_match()
                 
-    def update_round ( self , team1 , team2 , state ):
+    def update_round (self, team1, team2, state):
         # Stop the round if it is too long
-        if state.step > self.last_step + self.max_round_step :
+        if state.step > self.last_step + self.max_round_step:
             self.simu.end_round()
             
-    def get_res ( self ):
+    def get_res(self):
         return self.res
     
     def get_best(self):
